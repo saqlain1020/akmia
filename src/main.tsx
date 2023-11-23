4b4451920178dffe62665845c04268b9ed5c2fc5
Saqlain <saqlainprinters@gmail.com>
2023-11-23 08:42:03 +0500

    chore: deploy sub path config

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import theme from "./utils/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
